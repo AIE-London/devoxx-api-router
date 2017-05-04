8294178e5a51002fe712808737bbca8128a8655b
Eade <sam.eade@capgemini.com>
2017-05-04 17:24:58 +0100

    test refactored

--- test/app.test.js
+++ test/app.test.js
@@ -9,28 +9,26 @@ let request = require('supertest');
 let api = require('../app');
 
 
+describe('getUuid', function () {
 
-
-describe('getUuid', function(){
-
-    it('should return the UUID for user DanC', function(){
+    it('should return the UUID for user DanC', function () {
         request(api).get('/uuid?email=dan.cotton@capgemini.com')
             .set('Accept', 'text/plain')
             .expect(200)
-            .end(function(err, res) {
+            .end(function (err, res) {
                 expect(res.body).to.equal("26667c9fdcc603ee93b43fb3e780b07378695a86");
                 done();
             });
     })
 });
 
-describe('getScheduledTalks', function(){
+describe('getScheduledTalks', function () {
 
-    it('should return the scheduled talks for user DanC', function(){
+    it('should return the scheduled talks for user DanC', function () {
         request(api).get('/scheduled?uuid=26667c9fdcc603ee93b43fb3e780b07378695a86')
             .set('Accept', 'application/json')
             .expect(200)
-            .end(function(err, res) {
+            .end(function (err, res) {
                 expect(res.body).to.equal({
                     "favored": [
                         {
@@ -43,13 +41,13 @@ describe('getScheduledTalks', function(){
     })
 });
 
-describe('getFavoredTalks', function(){
+describe('getFavoredTalks', function () {
 
-    it('should return the favored talks for user DanC', function(){
+    it('should return the favored talks for user DanC', function () {
         request(api).get('/favored?uuid=26667c9fdcc603ee93b43fb3e780b07378695a86')
             .set('Accept', 'application/json')
             .expect(200)
-            .end(function(err, res) {
+            .end(function (err, res) {
                 expect(res.body).to.equal({
                     "scheduled": [
                         {
@@ -62,12 +60,12 @@ describe('getFavoredTalks', function(){
     })
 });
 
-describe('getSpeakerDetails', function() {
-    it('should return the speaker data for a talk', function() {
+describe('getSpeakerDetails', function () {
+    it('should return the speaker data for a talk', function () {
         request(api).get('/speakers?speakerId=695b40d928dd0a905b7ab1b900b5a5752870a7d8')
             .set('Accept', 'application/json')
             .expect(200)
-            .end((err,res) => {
+            .end((err, res) => {
                 expect(res.body).to.equal({
                     "uuid": "695b40d928dd0a905b7ab1b900b5a5752870a7d8",
                     "bioAsHtml": "<p>Helen has 20 years’ experience working in the technology industry with a focus on the Software Development Lifecycle for a wealth of cross industry clients in the UK and abroad. Helen is passionate about DevOps and is the creator of the Ranger4 DevOps LiftOff Workshop and the Ranger4 DevOps Maturity Assessment - winner of the IBM Beacon Award 2015 for Outstanding DevOps Solution. She also started Ranger4’s #DevOpsFriday5 initiative and is on the Board of Regents at the DevOps Institute (Ranger4 are also a Registered Education Provider for the DOI’s DevOps training courses). Helen is also a novelist and ecologist. She once saw a flamingo lay an egg.</p>\n",
@@ -99,17 +97,18 @@ describe('getSpeakerDetails', function() {
                     "avatarURL": "https://media.licdn.com/media/p/2/000/10f/320/3b9da1f.jpg",
                     "twitter": "@helenranger4",
                     "lang": "en"
-                })
-            })
+                });
+                done()
+            });
     });
 });
 
-describe('getTalkDetails', function() {
-    it('should return the talk data', function() {
+describe('getTalkDetails', function () {
+    it('should return the talk data', function () {
         request(api).get('/talks?talkId=IBN-5679')
             .set('Accept', 'application/json')
             .expect(200)
-            .end((err,res) => {
+            .end((err, res) => {
                 expect(res.body).to.equal({
                     "id": "IBN-5679",
                     "title": "The DevOps Superpattern",
@@ -128,7 +127,8 @@ describe('getTalkDetails', function() {
                             "name": "Helen Beal"
                         }
                     ]
-                })
-            })
+                });
+                done()
+            });
     });
 });
